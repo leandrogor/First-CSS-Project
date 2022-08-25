e8f54c169f8920c9cf7cbf9210e10bac6c3fe7ce
leandrogor <leandrogabrielgor@gmail.com>
2022-08-25 16:43:12 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+function calcEdad() {
+  actual = Number(prompt('¿Qué edad tenés?'));
+
+  while (isNaN(actual)) {
+    alert('Por favor, escribe un número')
+    actual = Number(prompt('¿Qué edad tenés?'));
+  }
+
+  alert('Dentro de 10 años tendrás ' + String(actual + 10) + '.');
+}
